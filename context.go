b5d71f006315e0689730f81fb8e22574cc033c19
Steven Wolfe <steven@swolfe.me>
2018-08-11 14:22:50 -0700

    Initial Commit

---
+++ context.go
@@ -0,0 +1,79 @@
+package gows
+
+import (
+	"encoding/json"
+	"net"
+
+	"github.com/gorilla/websocket"
+)
+
+// Context defines all functionality for handling a websocket message
+type Context interface {
+	// ConnID returns the ID of the client connection
+	ConnID() string
+
+	// RemoteAddr returns the net Address of the client connection
+	RemoteAddr() net.Addr
+
+	// Message returns the full message stored in the context
+	Message() string
+
+	// Bind JSON decodes the message on the context into the passed interface
+	Bind(i interface{}) error
+
+	// Send will write the passed string to the connection
+	Send(s string) error
+
+	// SendAll will write the passed string to all connections
+	SendAll(s string) error
+}
+
+// context contains all context about the websocket message
+
+type context struct {
+	server  *Server
+	conn    *websocket.Conn
+	connID  string
+	message []byte
+}
+
+// ConnID returns the ID of the client connection
+func (c *context) ConnID() string {
+	return c.connID
+}
+
+// RemoteAddr returns the net Address of the client connection
+func (c *context) RemoteAddr() net.Addr {
+	return c.conn.RemoteAddr()
+}
+
+// Message returns the full message stored in the context
+func (c *context) Message() string {
+	return string(c.message)
+}
+
+// Bind JSON decodes the message on the context into the passed interface
+func (c *context) Bind(i interface{}) error {
+	return json.Unmarshal(c.message, i)
+}
+
+// Send will write the passed string to the connection
+func (c *context) Send(s string) error {
+	return send(c.conn, s)
+}
+
+// SendAll will write the passed string to all connections
+func (c *context) SendAll(s string) error {
+	c.server.Conns.Range(func(_, value interface{}) bool {
+		if conn, ok := value.(*websocket.Conn); ok {
+			send(conn, s)
+		}
+		return true
+	})
+	return nil
+}
+
+// send will send the passed UTF8 message bytes to the passed connection
+func send(conn *websocket.Conn, msg string) error {
+	return conn.WriteMessage(websocket.TextMessage, []byte(msg))
+}
---
+++ gows.go
@@ -0,0 +1,109 @@
+// gows is a basic wrapper for gorillas websocket package. It makes it quick and
+// easy to write a websocket server using traditional http style request/message
+// handlers
+
+package gows
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
+	"github.com/tidwall/gjson"
+)
+
+// HandlerFunc is a type of function that is used for handling websocket
+// messages
+type HandlerFunc func(context Context) error
+
+// Server is a package of all required dependencies to run a gows websocket
+// server
+type Server struct {
+	Router   *mux.Router
+	Upgrader *websocket.Upgrader
+	Handlers map[string]HandlerFunc
+	Conns    *sync.Map
+}
+
+// New creates a new gows Server and binds the passed path
+func New(wsPath string) *Server {
+	// Create the new fully populated Server
+	s := &Server{
+		Router:   mux.NewRouter(),
+		Upgrader: &websocket.Upgrader{},
+		Handlers: make(map[string]HandlerFunc),
+		Conns:    &sync.Map{},
+	}
+
+	// Bind the websocket path and return the Server
+	s.Router.HandleFunc(wsPath, s.serveWs)
+	return s
+}
+
+// Handle adds a HandlerFunc to the map of websocket message handlers
+func (s *Server) Handle(name string, handler HandlerFunc) {
+	s.Handlers[name] = handler
+}
+
+// Static binds the passed directory path to the prefix path
+func (s *Server) Static(prefix, path string) {
+	fs := http.FileServer(http.Dir(path))
+	h := http.StripPrefix(prefix, fs)
+	s.Router.PathPrefix(prefix).Handler(h)
+}
+
+// Listen binds the standard websocket handler containing all handling logic
+// and begins listening for messages from connected websocket clients
+func (s *Server) Listen(addr string) error {
+	// Bind the websocket handler to the passed path
+	return http.ListenAndServe(addr, s.Router)
+}
+
+// serveWs is the primary websocket handler method which
+func (s *Server) serveWs(w http.ResponseWriter, r *http.Request) {
+	// Generate an ID for the new websocket client
+	connID := uuid.Must(uuid.NewV4()).String()
+
+	// Upgrade the connection for use with websockets
+	conn, err := s.Upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	s.Conns.Store(connID, conn)
+
+	// Defer close the connection and delete it from the conns map
+	defer s.Conns.Delete(connID)
+	defer conn.Close()
+
+	// For every message that comes through on the websocket
+	for {
+		// Read the next message on the connection
+		_, bmsg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		// JSON decode the type from the json formatted message
+		msgType := gjson.GetBytes(bmsg, "type").String()
+
+		// If a handler exists for the message type, handle it
+		if h, ok := s.Handlers[msgType]; ok {
+			if err := h(&context{
+				server:  s,
+				connID:  connID,
+				conn:    conn,
+				message: bmsg,
+			}); err != nil {
+				log.Println("handle:", err)
+				break
+			}
+		} else {
+			log.Println("Unsupported message:", string(bmsg))
+		}
+	}
+}
